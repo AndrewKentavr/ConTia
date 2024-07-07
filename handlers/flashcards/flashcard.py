d7aed121041ed37599e39ba425a273781032a988
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-07-08 02:02:35 +0500

    Deleted odd space

--- handlers/flashcards/flashcard.py
+++ handlers/flashcards/flashcard.py
@@ -6,7 +6,7 @@ from handlers.keyboards.default import flashcard_menu
 
 async def flashcard_start(message: types.Message, state: FSMContext):
     await state.finish()
-    await message.answer('<strong>"Начать учить карточки"</strong> - начнёт  процесс тренировки с карточками\n\n'
+    await message.answer('<strong>"Начать учить карточки"</strong> - начнёт процесс тренировки с карточками\n\n'
                          '<strong>"Управление карточками"</strong> - переправит в раздел для изменения карточек',
                          reply_markup=flashcard_menu.get_keyboard_flashcard_start())
 
